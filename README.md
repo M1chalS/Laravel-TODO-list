f43af44e84367d33a09dc312a40ada29c24b115c
M1chalS <michalszajner@wp.pl>
2022-12-14 22:39:16 +0100

    Upgrade of main page styling

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ This project is Simple TODO list made in Laravel with Tailwind css.
 ## Installation
 1. Make sure you have [sqlite](https://www.sqlite.org/index.html) installed
 2. Run `composer install` and then `npm install`
-3. When installation is ready run`php artisan migrate`
-4. Run `npm run dev` and `php artisan serve` to start local server
+3. Rename `.env.example` to `.env` 
+4. When installation is ready run`php artisan migrate`
+5. Run `npm run dev` and `php artisan serve` to start local server
 
 App by Michał Szajner
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.layout')
 
 @section('content')
-    <header class="w-full">
-        <h1 class="w-fit m-auto">Laravel TODO List</h1>
+    <header class="w-full p-4 text-blue-700">
+        <span class="absolute left-0 text-4xl"><i class="fa-sharp fa-solid fa-list-check" title="Twoje tablice"></i></span>
+        <h1 class="w-fit m-auto text-4xl ">Laravel TODO List</h1>
     </header>
-    <main class="w-full flex">
-        <div class="w-1/4 border-2">
+    <main class="w-full text-blue-400">
+        <section class="w-auto flex p-4">
+        <div class="w-1/4 border-2 border-blue-500">
             @foreach($lists as $list)
-                <div class="flex-wrap">{{$list->content}}
-                    <button type="submit" form="delete-{{$list->id}}"><i class="fa-sharp fa-solid fa-minus"></i>
-                    </button>
+                <div class="flex" x-data>
+                    <p class="flex-wrap break-all"
+                    @click="alert('list')"
+                    >{{$list->content}}</p>
+                    <button type="submit" form="delete-{{$list->id}}"><i class="fa-sharp fa-solid fa-minus"></i></button>
                 </div>
                 <form method="POST" action="/list/{{$list->id}}" id="delete-{{$list->id}}">
                     @csrf
@@ -28,7 +32,9 @@
                 </button>
             </form>
         </div>
+        </section>
     </main>
+
     <footer>
 
     </footer>
